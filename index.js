5c4359c07a6a929674d14f82a9b0a86e75944460
9090856788 <kanhucharansahoo595@gmail.com>
2025-01-04 00:54:59 +0530

    connect the mongoDB database to our nodejs application

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import dbConnection from "./db/dbConnection.js";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+dbConnection();
 
 app.listen(PORT, () => {
   console.log(`Server listening the PORT ${PORT}`);
